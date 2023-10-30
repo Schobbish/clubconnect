1f8818af84f7f37f9aeb7042aec5678e6d871f0c
Thach Huynh <thh190001@utdallas.edu>
2023-10-30 13:27:41 -0500

    started SearchResultsPage

---
+++ src/pages/SearchResultsPage.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import { ClubCard } from "../components/ClubCard";
+import { NavBar } from "../components/NavBar";
+import { apiAxios } from "../util/api";
+
+export interface SearchResultsProps {
+  searchQuery?: string;
+}
+
+export interface ClubData {
+  [name: string]: {
+    acronym: string;
+    president: string;
+    description: string;
+  };
+}
+
+export function SearchResults() {
+  const [clubData, setClubData] = useState<ClubData>({});
+
+  useEffect(() => {
+    apiAxios
+      .get("/api/clubs")
+      .then((res) => {
+        setClubData(res.data);
+      })
+      .catch(() => {
+        console.error("failed to get club data");
+      });
+  }, []);
+
+  return (
+    <div className="search-results">
+      <NavBar />
+      <div className="cards-container flex flex-wrap gap-5 pt-5">
+        {Object.keys(clubData).map((club) => (
+          <ClubCard clubName={club} key={club} />
+        ))}
+      </div>
+    </div>
+  );
+}
