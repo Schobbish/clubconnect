e1e7bf97857ffe24fcd576e36e809dc778b61ee2
Thach Huynh <thh190001@utdallas.edu>
2023-10-30 17:28:30 -0500

    rounded images and removed unused props

--- src/components/ClubCard.tsx
+++ src/components/ClubCard.tsx
@@ -4,13 +4,16 @@ import defaultLogo from "../images/Default Logo.png";
 export interface ClubCardProps {
   clubName?: string;
   clubLogo?: string;
-  clubAcronym?: string;
 }
 
 export function ClubCard(props: ClubCardProps) {
   return (
-    <div className="clubcard-container max-w-3xs ">
-      <img src={defaultTo(props.clubLogo, defaultLogo)} alt="gray box" />
+    <div className="clubcard-container max-w-3xs">
+      <img
+        className="rounded-xl"
+        src={defaultTo(props.clubLogo, defaultLogo)}
+        alt="gray box"
+      />
       <div className="clubname text-center">{props.clubName}</div>
     </div>
   );
