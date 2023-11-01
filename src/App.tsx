bc5b0614333960bdb4474e4e18e17cc2f7fac5c6
Thach Huynh <thh190001@utdallas.edu>
2023-11-01 02:39:38 -0500

    started club info page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Error404 } from "./pages/Error404";
 import { Home } from "./pages/Home";
 import { Search } from "./pages/Search";
+import { Club } from "./pages/Club";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
           <Route index element={<Home />} />
           <Route path="*" element={<Error404 />} />
           <Route path="/search" element={<Search />} />
+          <Route path="/club" element={<Club />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/pages/Club.tsx
@@ -0,0 +1,11 @@
+export function Club() {
+  return (
+    <div className="club-info-container">
+      This will contain the club information
+      <div className="club-contact-info-container">
+        This is where all the emails and socials should be contained (maybe some
+        icons)
+      </div>
+    </div>
+  );
+}
