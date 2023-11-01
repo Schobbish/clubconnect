ffaf280a86eb9e397bfc216638ccf194d9c74a3d
Nathaniel Adam <36092204+Schobbish@users.noreply.github.com>
2023-10-31 22:26:43 -0700

    create ClubResultsView

---
+++ src/components/ClubResultsView.tsx
@@ -0,0 +1,44 @@
+import { defaultTo, shuffle } from "lodash-es";
+import { ClubData } from "../models/ClubData";
+import { ClubCard } from "./ClubCard";
+import { ListViewClub } from "./ListViewClub";
+
+export interface ClubResultsViewProps {
+  title: string;
+  clubData: ClubData;
+  /** Array of club names to specify display order (default: random) */
+  order?: string[];
+}
+
+export function ClubResultsView(props: ClubResultsViewProps) {
+  const viewStyle = "Grid"; // eventually use a usestate
+  const inferredOrder = defaultTo(
+    props.order,
+    shuffle(Object.keys(props.clubData))
+  );
+
+  return (
+    <div className="px-2 pt-4 pb-12 mx-auto max-w-5xl">
+      <h1 className="font-bold text-3xl">{props.title}</h1>
+      <div className="cards-container flex flex-wrap gap-5 pt-5">
+        {viewStyle !== "Grid"
+          ? inferredOrder.map((clubName) => (
+              <ClubCard
+                clubName={clubName}
+                key={clubName}
+                clubLogo={props.clubData[clubName].logo}
+              />
+            ))
+          : inferredOrder.map((clubName) => (
+              <ListViewClub
+                key={clubName}
+                clubName={clubName}
+                clubDescription={props.clubData[clubName].description}
+                clubPresident={props.clubData[clubName].president}
+                clubLogo={props.clubData[clubName].logo}
+              />
+            ))}
+      </div>
+    </div>
+  );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,16 @@
 import { useEffect, useState } from "react";
-import { ClubCard } from "../components/ClubCard";
-import { ListViewClub } from "../components/ListViewClub";
+import { ClubResultsView } from "../components/ClubResultsView";
 import { NavBar } from "../components/NavBar";
 import { ClubData } from "../models/ClubData";
 import { apiAxios } from "../util/api";
 
 export function Home() {
   const [clubData, setClubData] = useState<ClubData>({});
-  const viewStyle = "Grid"; //eventually use a usestate
 
   useEffect(() => {
     apiAxios
       .get("/api/clubs")
       .then((res) => {
-        //console.log(Object.keys(res.data));
-        //console.log(Object.keys(res.data)[1]);
         setClubData(res.data);
       })
       .catch(() => {
@@ -25,28 +21,7 @@ export function Home() {
   return (
     <div className="home">
       <NavBar />
-      <div className="px-2 mx-auto max-w-5xl">
-        <h1>Top Clubs</h1>
-        <div className="cards-container flex flex-wrap gap-5 pt-5">
-          {viewStyle !== "Grid"
-            ? Object.keys(clubData).map((club) => (
-                <ClubCard
-                  clubName={club}
-                  key={club}
-                  clubLogo={clubData[club].logo}
-                />
-              ))
-            : Object.keys(clubData).map((club) => (
-                <ListViewClub
-                  key={club}
-                  clubName={club}
-                  clubDescription={clubData[club].description}
-                  clubPresident={clubData[club].president}
-                  clubLogo={clubData[club].logo}
-                />
-              ))}
-        </div>
-      </div>
+      <ClubResultsView title="Top Clubs" clubData={clubData} />
     </div>
   );
 }
