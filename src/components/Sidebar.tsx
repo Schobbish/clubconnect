b2df13bbc5f3f434e0b05b47cf5ec2107be8f049
Thach Huynh <thh190001@utdallas.edu>
2023-11-02 02:00:52 -0500

    typography stuff

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,9 +2,9 @@ export function Sidebar() {
   return (
     <div className="sidebar-container border-l-2 sticky top-0 float-right h-screen w-2/12 ">
       <br />
-      <div className="sticky">
-        <div className="text-center font-bold w-full">Filter</div>
-        <div className="text-center font-bold w-full">Scheduler</div>
+      <div className="sticky text-center font-bold w-full text-3xl underline">
+        <p>Filter</p>
+        <p>Scheduler</p>
       </div>
     </div>
   );
