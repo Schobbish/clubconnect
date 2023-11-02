e63585715d9ceff7d79aafb685c3fd310ff4588e
Thach Huynh <thh190001@utdallas.edu>
2023-11-02 01:26:28 -0500

    adjusting list items with sidebar

--- src/components/ClubListItem.tsx
+++ src/components/ClubListItem.tsx
@@ -9,20 +9,22 @@ export interface ClubListItemProps {
   president?: string;
 }
 
+//Need to adjust logo size and truncating text for when the list view gets a certain height
+
 export function ClubListItem(props: ClubListItemProps) {
   return (
     <Link to={"/club?" + createSearchParams({ name: props.name })}>
       <div className="listclub-container flex flex-wrap">
         <img
-          className="max-w-3xs rounded-xl"
+          className="max-w-3xs rounded-xl object-cover"
           src={inferLogoSource(props.logo)}
           alt={props.name + " logo"}
         />
-        <div className="club-information pl-10">
+        <div className="club-information pl-10 ">
           <div className="clubName underline font-bold text-2xl">
             {props.name}
           </div>
-          <div className="clubDescription max-w-3xl pt-2 pb-2">
+          <div className="clubDescription max-w-xl pt-2 pb-2">
             {props.description}
           </div>
           <div className="clubPresident underline">
