9e21586525d4efaf6d7f88f529e37b60bc034da9
Thach Huynh <thh190001@utdallas.edu>
2023-11-09 00:30:57 -0600

    added calendar stuff (ty Nathan)

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,15 +1,49 @@
-import { weekOrder } from "../models/meetingTypes";
+import { weekOrder, MeetingSchedule } from "../models/meetingTypes";
 
-export function Calendar() {
+interface CalendarProps {
+  meetingSchedule: MeetingSchedule;
+}
+
+export function Calendar(props: CalendarProps) {
   return (
     <div className="calendar-outer border-2">
       <div className="calendar-month text-center font-bold text-3xl border-b-2">
         November
       </div>
-      <div className="calendar-daysOfWeek grid grid-cols-7 divide-x">
+      <div className="calendar-daysOfWeek border-b-2 grid grid-cols-7 divide-x">
         {weekOrder.map((dayOfWeek) => (
           <div key={dayOfWeek} className="text-center">
             {dayOfWeek}
+          </div>
+        ))}
+      </div>
+      <div className="calendar-events grid grid-cols-7 divide-x">
+        {weekOrder.map((dayOfWeek) => (
+          <div key={dayOfWeek} className="text-center h-[127.5px]">
+            <div className="font-bold">
+              {props.meetingSchedule[dayOfWeek]?.map((val) => val.name)}
+            </div>
+            {props.meetingSchedule[dayOfWeek]?.map(
+              (val) =>
+                "Starts: " +
+                (Math.floor(val.startTime / 60) > 12
+                  ? Math.floor(val.startTime / 60) - 12
+                  : Math.floor(val.startTime / 60)) +
+                ":" +
+                (val.startTime % 60 == 0 ? "00" : val.startTime % 60) +
+                (Math.floor(val.startTime / 60) > 12 ? "PM" : "AM")
+            )}
+            <br />
+            {props.meetingSchedule[dayOfWeek]?.map(
+              (val) =>
+                "Ends: " +
+                (Math.floor(val.endTime / 60) > 12
+                  ? Math.floor(val.endTime / 60) - 12
+                  : Math.floor(val.endTime / 60)) +
+                ":" +
+                (val.endTime % 60 == 0 ? "00" : val.endTime % 60) +
+                (Math.floor(val.endTime / 60) > 12 ? "PM" : "AM")
+            )}
           </div>
         ))}
       </div>
