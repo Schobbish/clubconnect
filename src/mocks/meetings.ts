c230566f9ccb74278c89554e34d74d997d2cb9b1
Thach Huynh <thh190001@utdallas.edu>
2023-11-09 00:54:59 -0600

    separate file for all meetings/events

---
+++ src/mocks/meetings.ts
@@ -0,0 +1,41 @@
+import { MeetingSchedule } from "../models/meetingTypes";
+
+export const meetings: MeetingSchedule = {
+  Sunday: [
+    {
+      name: "blah blah",
+      startTime: 1170,
+      endTime: 1260
+    }
+  ],
+  Monday: [
+    {
+      name: "yurt",
+      startTime: 1170,
+      endTime: 1260
+    }
+  ],
+  Tuesday: [],
+  Wednesday: [
+    {
+      name: "blah blah",
+      startTime: 970,
+      endTime: 1260
+    }
+  ],
+  Thursday: [
+    {
+      name: "blah blah",
+      startTime: 170,
+      endTime: 1260
+    }
+  ],
+  Friday: [
+    {
+      name: "blah",
+      startTime: 1010,
+      endTime: 1260
+    }
+  ],
+  Saturday: []
+};
