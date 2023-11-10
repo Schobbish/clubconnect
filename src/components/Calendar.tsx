1cd016518146e967e3fd6f95071e56ba0eaf70a8
Thach Huynh <thh190001@utdallas.edu>
2023-11-10 00:57:51 -0600

    adjust getting events (all or for a club)

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -3,6 +3,46 @@ import { convertMinutesToTime } from "../util/misc";
 
 interface CalendarProps {
   meetingSchedule: MeetingSchedule;
+  clubName?: string;
+}
+
+function getAllEvents(props: CalendarProps) {
+  return (
+    <div className="calendar-events grid grid-cols-7 divide-x">
+      {weekOrder.map((dayOfWeek) => (
+        <div key={dayOfWeek} className="text-center h-[127.5px]">
+          <div className="font-bold">
+            {props.meetingSchedule[dayOfWeek]?.map((val) => val.name)}
+          </div>
+
+          {props.meetingSchedule[dayOfWeek]?.map(
+            (val) => "Starts: " + convertMinutesToTime(val.startTime)
+          )}
+          <br />
+
+          {props.meetingSchedule[dayOfWeek]?.map(
+            (val) => "Ends: " + convertMinutesToTime(val.endTime)
+          )}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+function getClubEvents(props: CalendarProps, clubName: string) {
+  return (
+    <div className="calendar-events grid grid-cols-7 divide-x">
+      {weekOrder.map((dayOfWeek) => (
+        <div key={dayOfWeek} className="text-center h-[127.5px]">
+          {props.meetingSchedule[dayOfWeek]?.map((val) => (
+            <div key={val.clubName} className="font-bold">
+              {val.clubName === clubName ? val.name : " "}
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export function Calendar(props: CalendarProps) {
@@ -18,22 +58,9 @@ export function Calendar(props: CalendarProps) {
           </div>
         ))}
       </div>
-      <div className="calendar-events grid grid-cols-7 divide-x">
-        {weekOrder.map((dayOfWeek) => (
-          <div key={dayOfWeek} className="text-center h-[127.5px]">
-            <div className="font-bold">
-              {props.meetingSchedule[dayOfWeek]?.map((val) => val.name)}
-            </div>
-            {props.meetingSchedule[dayOfWeek]?.map(
-              (val) => "Starts: " + convertMinutesToTime(val.startTime)
-            )}
-            <br />
-            {props.meetingSchedule[dayOfWeek]?.map(
-              (val) => "Ends: " + convertMinutesToTime(val.endTime)
-            )}
-          </div>
-        ))}
-      </div>
+      {props.clubName
+        ? getClubEvents(props, props.clubName)
+        : getAllEvents(props)}
     </div>
   );
 }
