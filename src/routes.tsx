f6c52f2104c510678a83e2dbfa72874b596c6540
Thach Huynh <thh190001@utdallas.edu>
2023-11-10 02:43:53 -0600

    add calendar route

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,12 +5,14 @@ import { Club } from "./pages/Club";
 import { Error404 } from "./pages/Error404";
 import { Home } from "./pages/Home";
 import { Search } from "./pages/Search";
+import { CalendarResults } from "./pages/CalendarResults";
 
 export const routes = createRoutesFromElements(
   <Route path="/" element={<App />}>
     <Route index element={<Home />} />
     <Route path="/club" element={<Club />} />
     <Route path="/search" element={<Search />} />
+    <Route path="/calendar" element={<CalendarResults />} />
     <Route path="*" element={<Error404 />} />
   </Route>
 );
