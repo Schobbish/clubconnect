f0eb3e45ce9ad5b1536243c609e7272d18c0caed
harrytang6449 <harrytang6449@gmail.com>
2023-11-11 16:46:08 -0600

    add clubs and update their schedule

--- src/mocks/clubJson.ts
+++ src/mocks/clubJson.ts
@@ -1,6 +1,19 @@
 import { ClubJson } from "../models/clubTypes";
 
 export const clubJson: ClubJson = {
+  "200percent": {
+    acronym: "",
+    president: "Cheri Tang",
+    description:
+      "The purpose of 200percent, as a K-pop performance group, is to provide the students of UT Dallas with the opportunity to participate in learning and performing various K-pop dances. Our covers are filmed and uploaded onto our YouTube channel. 200% also have the opportunity to not only collaborate with other Kpop dance groups but also perform at live events such as the annual Korean Festival and the Mid-Autumn Festival. Our mission is to provide a safe, comfortable community of shared interest and cultural diversity of UT Dallas and its students.",
+    logo: "/images/200percent_logo.jpg",
+    categories: ["Art", "Hobby"],
+    socials: {
+      Instagram: "https://www.instagram.com/__200percent__/",
+      Facebook: "https://www.facebook.com/200PERCENTDANCE/"
+    }
+  },
+
   "Accessible Prosthetics Initiative UT Dallas Chapter": {
     acronym: "API",
     president: "Joshua Price",
@@ -38,7 +51,7 @@ export const clubJson: ClubJson = {
     description:
       "AMED’s mission is to enlighten college students about modern medical advancements by providing leadership and support for those on the pre-health journey.",
     logo: "/images/AMED_logo.png",
-    categories: ["Academic"]
+    categories: ["Academic", "Health"]
   },
 
   "Afro Hair Association": {
@@ -47,7 +60,32 @@ export const clubJson: ClubJson = {
     description:
       "To support afro-textured hair, so everyone can be comfortable with the hair they were born with. As well as supporting our communities & socializing with others to spread awareness. Although it’s called the Afro Hair Association, we are not going be just be about hair. We intend to use the organization as a way to help those in need, clean the community, uplift the community through supporting one another, and remove the stigma surrounding Afro-textured hair in the workplace and schools.",
     logo: "/images/afro_logo.png",
-    categories: ["Cultural"]
+    categories: ["Cultural", "Hobby"]
+  },
+
+  Anim3: {
+    acronym: "",
+    president: "Preston Wong",
+    description:
+      "We at A3 are the official anime club of UT Dallas. We welcome all UTD students, staff, alumni, or acquaintances of the aforementioned groups. Although anyone can watch anime in the comfort of their own homes, watching with others is also a great way to enjoy shows! We at A3 feel this sense of camaraderie is one of our greatest strengths. Everyone tends to have a wide array of tastes, yet we all come together to enjoy the same shows. Aside from shows, our other activities include Kahoot trivia nights, Iron Cosplay, local events, anime merch swap/trade meets, potlucks, etc. We’re always open to new ideas for meetings as well so feel free to participate in the discussion. Now you may be wondering, why A3? The 3 stands for our three tenets: culture, social, and education. We seek to do what other anime clubs don't. We explore the culture in anime and integrate it into meetings and host grounds for making friends and establishing bonds. Not only can we take the time to appreciate anime and manga for what it is, but we can also take the time to dissect them and see how to better the lives of others as well.”",
+    logo: "/images/anim3_logo.jpeg",
+    categories: ["Hobby"],
+    socials: {
+      Facebook: "https://www.facebook.com/groups/utda3.2016.2017/"
+    }
+  },
+
+  "Anime Orchestra Ensemble": {
+    acronym: "AOE",
+    president: "Adam Shaker",
+    description:
+      "AOE is a student led orchestra which focuses on anime and video game music. Our mission is: “To give people the opportunity to perform non-traditional orchestral music primarily sourced from anime and video games.” and “To provide a vibrant community within which fans of various series can meet, connect, and further explore their favorite genres.”",
+    logo: "/images/aoe_logo.jpg",
+    categories: ["Art", "Hobby"],
+    socials: {
+      Instagram: "https://www.instagram.com/aoedallas/?hl=en",
+      Facebook: "https://www.facebook.com/groups/AnimeOrchestraEnsemble/"
+    }
   },
 
   "Asian Student Organization": {
@@ -85,7 +123,7 @@ export const clubJson: ClubJson = {
     description:
       "UT Dallas Chess Club is a place where you can meet every week to improve your chess skills, attend tournaments and events, participate in grandmaster lessons and simuls, or just play a game of chess with your friend",
     logo: "/images/chess_logo.png",
-    categories: ["Sport"],
+    categories: ["Sport", "Hobby"],
     socials: {
       Instagram: "https://www.instagram.com/utdchess/",
       Facebook: "https://www.facebook.com/utdchess/",
@@ -136,6 +174,19 @@ export const clubJson: ClubJson = {
     }
   },
 
+  "Fashion Club": {
+    acronym: "",
+    president: "Sohila Elfar",
+    description:
+      "The Fashion Club is a safe haven for creativity and self-expression through fashion. Our mission is to offer a platform for designers and models to express their talents. We work on connecting members with fashion professionals, teach those interested about the fashion industry, and host social events.",
+    logo: "/images/fashion_club_logo.png",
+    categories: ["Social"],
+    socials: {
+      Instagram: "https://www.instagram.com/utd_fashion/",
+      Facebook: "https://www.facebook.com/FashionClubatUTD/"
+    }
+  },
+
   "Filipino Student Association": {
     acronym: "FSA",
     president: "Sabrina Uy-Tesy",
@@ -150,6 +201,101 @@ export const clubJson: ClubJson = {
     }
   },
 
+  "Fit Life UTD": {
+    acronym: "",
+    president: "Maamoon Mian",
+    description:
+      "We are a club founded on the principle of promoting health and fitness by serving and educating the community, hosting events that encourage members to maintain a healthy lifestyle, and fundraising events which allows others to have access to equipment and facilities to maintain their health. By spreading awareness through many platforms such as in-person fitness events, group runs, hikes, and workouts, and social media we can pique the public’s interest in our mission. Giving back to the community is another piece of what this organization represents. Our organization will be hosting events in which canned food drives, supply drives, and volunteer work options are going to be available for all members who want to make a difference within themselves and a difference in the community we all live in.",
+    logo: "/images/fit_utd_logo.png",
+    categories: ["Social", "Health"]
+  },
+
+  "Hearts for the Homeless UTD": {
+    acronym: "H4H",
+    president: "Venkata Sree Harini Repaka",
+    description:
+      "Hearts for the Homeless UTD seeks to contribute to the health and well-being of one of the most vulnerable populations in our nation through free blood pressure screening, education, and outreach opportunities for students who wish to contribute to our effort. Hearts for the Homeless (H4H) UTD serves to provide UTD students with a unique volunteering experience. Our mission is to enlighten and impassion students who volunteer their time and skills for a greater cause. Furthermore, it functions to aid the homeless community in Dallas by providing free blood pressure screenings and fostering caring interactions between volunteers, homeless individuals, and others in the community. Hearts for the Homeless UTD plans to collaborate with other non-profit organizations, veteran's organizations, health care clinics, and medical professionals to enhance the effectiveness of this registered student organization. We also seek to educate members on cardiovascular disease and hidden difficulties in the homeless community and thus aspire to help build socially-cognizant and empathetic leaders driven to create change in their communities.",
+    logo: "/images/H4H_logo.png",
+    categories: ["Social"],
+    socials: {
+      Instagram: "https://www.instagram.com/h4h.dallas.unt/?hl=en",
+      Facebook: "https://www.facebook.com/H4HDallas/"
+    }
+  },
+
+  "International Christian Fellowship (ICF)": {
+    acronym: "ICF",
+    president: "Uchechukwu Nzedibe",
+    description:
+      "The mission of ICF is to share the love of Christ with international students.",
+    logo: "/images/icf_logo.jpg",
+    categories: ["Religion"],
+    socials: {
+      Instagram: "https://www.instagram.com/utdallasicf/?hl=en",
+      Facebook: "https://www.facebook.com/groups/202515916458570/"
+    }
+  },
+
+  "Jujutsu Club": {
+    acronym: "",
+    president: "Virginia Cheng",
+    description:
+      "The Jujutsu Club is an organization that teaches Japanese jujutsu, a self defense martial art. We aspire to make self defense, a vital lesson for everyone, to be free of charge, safe, and accessible for all.",
+    logo: "/images/jujutsu_club_logo.png",
+    categories: ["Sport"]
+  },
+
+  "Neuroscience Student Association": {
+    acronym: "NSA",
+    president: "Grace Moore",
+    description:
+      "The UTD Neuroscience Student Association (NSA) is a place where students of any major or background can come together over their shared love of neuroscience. We aim to provide a venue for learning, seeking advice, gaining new experiences, and making friends. With members from all walks of life and with a variety of academic and career goals, our community has something for everyone.",
+    logo: "/images/nsa_logo.png",
+    categories: ["Academic", "Health"],
+    socials: {
+      Instagram: "https://www.instagram.com/utdnsa/?hl=en",
+      Facebook: "https://www.facebook.com/groups/utdnsa/"
+    }
+  },
+
+  "Pre-Physician Assistant Society": {
+    acronym: "PPAS",
+    president: "Shanaya Mohammed",
+    description:
+      "The Pre-Physician Assistant Society (PPAS) is a student-led organization at UTD committed to preparing Pre-PA students towards their future career. By providing academic resources, encouraging volunteerism, and supplying healthcare/patient care experience opportunities, the PPAS readies students for the next step in their Physician Assistant endeavors.",
+    logo: "/images/PPAS_logo.png",
+    categories: ["Academic", "Health"],
+    socials: {
+      Instagram: "https://www.instagram.com/utdprepa/?hl=en",
+      Facebook: "https://www.facebook.com/utdprepa/"
+    }
+  },
+
+  "Reptile Club of UTD": {
+    acronym: "",
+    president: "Bridgette Bryant",
+    description:
+      "We are a passionate group of individuals with a love of reptiles, amphibians, and other exotics alike. Our goal is to provide a social interest group, a place for students to get information about exotic animals, and help spread awareness of wildlife conservation, rescue, and care. As an organization our donations and funds will contribute to the organization's events, materials needed, and be donated to Nature's Edge Wildlife and Reptile Rescue in Fort Worth Texas. Making us a mix of social, service, and education as an organization. If you love reptiles/wildlife, or are just curious, come check us out!",
+    logo: "/images/reptile_club_logo.jpg",
+    categories: ["Hobby"],
+    socials: {
+      Facebook: "https://www.facebook.com/ReptileClubUTD/"
+    }
+  },
+
+  "Rocket League Club": {
+    acronym: "",
+    president: "Owais muhammad",
+    description:
+      "To promote and foster a community around Rocket League at UTD",
+    logo: "/images/rocket_league_logo.png",
+    categories: ["Hobby", "Gaming"],
+    socials: {
+      Instagram: "https://www.instagram.com/utdrl/?hl=en",
+      Facebook: "https://www.facebook.com/UTDRL/"
+    }
+  },
+
   "Society of Asian Scientists and Engineers": {
     acronym: "SASE",
     president: "Shreya TK",
@@ -160,6 +306,39 @@ export const clubJson: ClubJson = {
     socials: {
       Instagram: "https://www.instagram.com/sase.utd/",
       Discord: "https://discord.com/invite/xzRkND3qfK"
+    }
+  },
+
+  "Stop Pretending You Can't Dance": {
+    acronym: "SPYCD",
+    president: "Megan Cooley",
+    description:
+      "Stop Pretending You Can't Dance (SPYCD) is a university dance club aimed at promoting Latin and Ballroom dances among UTD students. We offer lessons that are beginner friendly but also progressive in difficulty in order to provide healthy challenges and enjoyment to our members",
+    logo: "/images/dance_logo.jpg",
+    categories: ["Hobby", "Art"],
+    socials: {
+      Instagram: "https://www.instagram.com/utd_spycd/?hl=en"
+    }
+  },
+
+  "The Drawing Sessions": {
+    acronym: "",
+    president: "Eesha Muddasani",
+    description:
+      "The Drawing Sessions is a multi-disciplinary fine arts club whose purpose is to celebrate drawing as a shared interest. It facilitates a productive atmosphere, a positive social environment, and additional networking opportunities for students of all majors. From all backgrounds and skill levels, we meet to hone our craft, make new friends, and celebrate drawing! We primarily focus on figure drawing with clothed, volunteer models, but we also do various art exercises and activities in our weekly sessions",
+    logo: "/images/drawing_club_logo.png",
+    categories: ["Hobby", "Art"]
+  },
+
+  UTDuelists: {
+    acronym: "",
+    president: "Ryan Ly",
+    description:
+      "To provide students on campus a local place to explore their interests and hobbies and learn about the card game, “Yu-Gi-Oh!” with other students in a friendly manner without needing to buy cards or drive to card shops in both the casual and competitive styles of the game.",
+    logo: "/images/utduelist_logo.png",
+    categories: ["Hobby", "Gaming"],
+    socials: {
+      Instagram: "https://www.instagram.com/utduelists/?hl=en"
     }
   },
 
--- src/mocks/meetings.ts
+++ src/mocks/meetings.ts
@@ -1,14 +1,76 @@
 import { MeetingSchedule } from "../models/meetingTypes";
 
 export const meetings: MeetingSchedule = {
-  Sunday: [],
-  Monday: [],
+  Sunday: [
+    {
+      name: "Fashion Talk",
+      clubName: "Fashion Club",
+      startTime: "10:00",
+      endTime: "11:00"
+    },
+    {
+      name: "Friendly Show Match",
+      clubName: "Chess Club UT Dallas",
+      startTime: "9:00",
+      endTime: "12:00"
+    },
+    {
+      name: "Free Afro Haircut",
+      clubName: "Afro Hair Association",
+      startTime: "13:30",
+      endTime: "15:30"
+    },
+    {
+      name: "General Meeting",
+      clubName: "Anime Orchestra Ensemble",
+      startTime: "14:00",
+      endTime: "16:00"
+    },
+    {
+      name: "Time To Duel",
+      clubName: "UTDuelists",
+      startTime: "14:00",
+      endTime: "16:00"
+    }
+  ],
+  Monday: [
+    {
+      name: "General Meeting",
+      clubName: "Neuroscience Student Association",
+      startTime: "14:00",
+      endTime: "15:30"
+    },
+    {
+      name: "General Meeting",
+      clubName: "Actuarial Student Association",
+      startTime: "14:00",
+      endTime: "15:30"
+    },
+    {
+      name: "General Meeting",
+      clubName: "Pre-Physician Assistant Society",
+      startTime: "14:30",
+      endTime: "16:00"
+    },
+    {
+      name: "Gym Campus Workout",
+      clubName: "Fit Life UTD",
+      startTime: "19:30",
+      endTime: "21:00"
+    }
+  ],
   Tuesday: [
     {
       name: "General Meeting",
       clubName: "Filipino Student Association",
       startTime: "19:30",
       endTime: "21:30"
+    },
+    {
+      name: "Lab Meeting",
+      clubName: "Association for Computing Machinery",
+      startTime: "18:00",
+      endTime: "19:30"
     },
     {
       name: "General Meeting",
@@ -18,13 +80,62 @@ export const meetings: MeetingSchedule = {
     },
     {
       name: "General Meeting",
+      clubName: "Society of Asian Scientists and Engineers",
+      startTime: "15:30",
+      endTime: "17:30"
+    },
+    {
+      name: "General Meeting",
       clubName: "Asian Student Organization",
       startTime: "16:00",
       endTime: "18:30"
     }
   ],
-  Wednesday: [],
+  Wednesday: [
+    {
+      name: "General Meeting",
+      clubName: "Hearts for the Homeless UTD",
+      startTime: "10:00",
+      endTime: "11:30"
+    },
+    {
+      name: "General Meeting",
+      clubName: "International Christian Fellowship (ICF)",
+      startTime: "17:00",
+      endTime: "19:00"
+    },
+    {
+      name: "General Meeting",
+      clubName: "Neuroscience Student Association",
+      startTime: "14:00",
+      endTime: "15:30"
+    },
+    {
+      name: "Weekly Jujutsu Session",
+      clubName: "Jujutsu Club",
+      startTime: "17:30",
+      endTime: "18:30"
+    }
+  ],
   Thursday: [
+    {
+      name: "General Meeting",
+      clubName: "Advancements in Modern Medicine",
+      startTime: "10:00",
+      endTime: "11:30"
+    },
+    {
+      name: "General Meeting",
+      clubName: "Accessible Prosthetics Initiative UT Dallas Chapter",
+      startTime: "17:00",
+      endTime: "19:00"
+    },
+    {
+      name: "Weekly Offline Game",
+      clubName: "Rocket League Club",
+      startTime: "17:00",
+      endTime: "19:00"
+    },
     {
       name: "General Meeting",
       clubName: "Vietnamese Student Association",
@@ -34,11 +145,60 @@ export const meetings: MeetingSchedule = {
   ],
   Friday: [
     {
+      name: "Threats and Attack Protection",
+      clubName: "Cybersecurity Club",
+      startTime: "15:00",
+      endTime: "17:00"
+    },
+    {
+      name: "K-pop Dance Practice",
+      clubName: "200percent",
+      startTime: "18:00",
+      endTime: "20:00"
+    },
+    {
+      name: "General Meeting",
+      clubName: "Chinese Student Association",
+      startTime: "19:00",
+      endTime: "21:00"
+    },
+    {
       name: "General Meeting",
       clubName: "Filipino Student Association",
       startTime: "19:30",
       endTime: "21:30"
     }
   ],
-  Saturday: []
+  Saturday: [
+    {
+      name: "Standup Comedy Show",
+      clubName: "Comet Comedy",
+      startTime: "10:00",
+      endTime: "12:00"
+    },
+    {
+      name: "Pet Reptile Meet",
+      clubName: "Reptile Club of UTD",
+      startTime: "18:00",
+      endTime: "19:30"
+    },
+    {
+      name: "Free Blood Test Camp",
+      clubName: "Hearts for the Homeless UTD",
+      startTime: "17:00",
+      endTime: "20:00"
+    },
+    {
+      name: "Anime Binge Watch",
+      clubName: "Anim3",
+      startTime: "14:00",
+      endTime: "16:00"
+    },
+    {
+      name: "Meet and Dance",
+      clubName: "Stop Pretending You Can't Dance",
+      startTime: "18:00",
+      endTime: "20:00"
+    }
+  ]
 };
