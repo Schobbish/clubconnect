61fce8c786cdfb8725b46bcbd8b807de27408d1a
Nathaniel Adam <36092204+Schobbish@users.noreply.github.com>
2023-12-05 18:38:57 -0600

    improve logic of all clubs dialog

--- src/components/AllClubsDialog.tsx
+++ src/components/AllClubsDialog.tsx
@@ -1,5 +1,6 @@
+import { isEmpty } from "lodash-es";
 import { useContext } from "react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { CategoryFilter } from "./CategoryDialog";
 import { Dialog, DialogProps } from "./Dialog";
 import { ScheduleFilter } from "./ScheduleDialog";
@@ -7,21 +8,25 @@ import { ScheduleFilter } from "./ScheduleDialog";
 export type AllClubsDialogProps = Omit<DialogProps, "className" | "children">;
 
 export function AllClubsDialog(props: AllClubsDialogProps) {
-  const [, setCategoryFilter] = useContext(CategoryFilter);
+  const setCategoryFilter = useContext(CategoryFilter)[1];
   const [scheduleFilter, setScheduleFilter] = useContext(ScheduleFilter);
   const navigate = useNavigate();
+  const location = useLocation();
 
   return (
     <Dialog className="clear-filters-dialog w-full max-w-xs" {...props}>
-      <h3>This disables all filters</h3>
+      <h3>This disables active filters</h3>
       <p className="mb-2">Proceed?</p>
       <button
         className="button-primary mr-2"
         type="button"
         onClick={() => {
           setCategoryFilter([]);
-          setScheduleFilter({ ...scheduleFilter, enabled: false });
-          navigate("/search");
+          // don't disable if empty
+          if (!isEmpty(scheduleFilter.filter))
+            setScheduleFilter({ ...scheduleFilter, enabled: false });
+          // don't navigate away from calendar
+          if (location.pathname !== "/calendar") navigate("/search");
           props.onClose();
         }}
       >
